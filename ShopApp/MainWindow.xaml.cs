3023e97fe757fbd6f2a501e939db6b8c887cc818
User <User@ASPIRE-5>
2026-04-14 17:14:34 +0300

    Add project files.

---
+++ ShopApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace ShopApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddItem_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            string price = txtPrice.Text;
+
+            if (name != "" && price != "")
+            {
+                listBox.Items.Add($"Товар: {name} | Ціна: {price} грн");
+
+                txtName.Clear();
+                txtPrice.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Заповни всі поля!");
+            }
+        }
+
+        private void DeleteItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedItem != null)
+            {
+                listBox.Items.Remove(listBox.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Обери товар для видалення!");
+            }
+        }
+    }
+}
